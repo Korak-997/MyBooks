09918e7f3d035d2bfbc847ec79fa64f256e870c1
Korak Kurani <korak.kurani@sincnovation.com>
2024-01-23 10:28:20 +0100

    Add books

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script>
 	import Header from '../components/Header.svelte';
 	import './styles.css';
+	import BookStore from '../stores/Store';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		fetch('/books.json')
+			.then((response) => response.json())
+			.then((json) => {
+				BookStore.update(() => {
+					return json;
+				});
+			});
+	});
 </script>
 
 <div class="app">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,35 @@
 <script>
+	import BooksStore from '../stores/Store';
+	import dummyCover from '$lib/images/dummy-cover.png';
+	let books;
+	import Icon from '@iconify/svelte';
+	BooksStore.subscribe((data) => {
+		books = data;
+	});
 </script>
 
 <svelte:head>
 	<title>MyBooks | Books</title>
 	<meta name="description" content="A personal Library" />
 </svelte:head>
+
+<div class="flex items-center justify-around gap-6 w-11/12 p-4 flex-wrap">
+	{#each books as book}
+		<div class="card w-96 bg-base-100 shadow-xl shadow-black">
+			<figure class="px-10 pt-10">
+				<img src={book.cover || dummyCover} alt="Shoes" class="rounded-xl" />
+			</figure>
+			<div class="card-body items-center text-center">
+				<h2 class="card-title text-2xl text-primary">{book.title}</h2>
+				<h3 class="card-title text-xl text-secondary">By: {book.author}</h3>
+				<div class="w-11/12">
+					<p>{book.description.substring(0, 100) + ' ...'}</p>
+				</div>
+				<div class="card-actions">
+					<button class="btn btn-info"><Icon icon="mdi:show" class="text-4xl" /></button>
+					<button class="btn btn-warning"><Icon icon="tabler:edit" class="text-4xl" /></button>
+				</div>
+			</div>
+		</div>
+	{/each}
+</div>
