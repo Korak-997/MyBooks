847dbbfe2a7b6896418f277e1e8be52ab5c6babc
Korak Kurani <korak.kurani@sincnovation.com>
2024-01-23 09:08:32 +0100

    Add header

---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Logo from '$lib/images/logo.png';
+</script>
+
+<header>
+	<div class="navbar bg-base-100 shadow-md shadow-black">
+		<div class="navbar-start">
+			<a class="hover:shadow-2xl" href="/"><img src={Logo} alt="Logo" class="w-16" /></a>
+		</div>
+		<div class="navbar-center flex">
+			<ul class="menu menu-horizontal px-1 text-2xl">
+				<li><a class="hover:text-secondary" href="/">Books</a></li>
+				<li><a class="hover:text-secondary" href="/authors">Authors</a></li>
+			</ul>
+		</div>
+		<div class="navbar-end"></div>
+	</div>
+</header>
