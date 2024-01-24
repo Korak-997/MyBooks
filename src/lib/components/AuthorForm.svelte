bfffd658d3d09cc49ae69857d71bdd87a41f4437
Korak Kurani <korak.kurani@sincnovation.com>
2024-01-24 14:21:25 +0100

    Add funtionality to cancel button

--- src/lib/components/AuthorForm.svelte
+++ src/lib/components/AuthorForm.svelte
@@ -118,7 +118,7 @@
 </label>
 
 <div class="flex items-center justify-around w-11/12 shadow-xl shadow-black p-4 flex-wrap">
-	<button class="btn btn-error"><Icon icon="iconoir:cancel" class="text-2xl" /></button>
+	<a href="/authors" class="btn btn-error"><Icon icon="iconoir:cancel" class="text-2xl" /></a>
 	<button on:click={saveAuthor} class="btn btn-success"
 		><Icon icon="material-symbols:save-outline" class="text-2xl" /></button
 	>
