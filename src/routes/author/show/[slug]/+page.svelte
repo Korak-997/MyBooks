66b0a3e51a39c6e6544144bf861fbc92ae6b8cd1
dani-lin-desk <dani.linux.97@gmail.com>
2024-01-25 20:45:37 +0100

    Add better styles for author show

--- src/routes/author/show/[slug]/+page.svelte
+++ src/routes/author/show/[slug]/+page.svelte
@@ -21,27 +21,30 @@
 	<meta name="description" content="A personal Library" />
 </svelte:head>
 <div class="flex items-center justify-center flex-col gap-6 w-11/12">
-	<h1 class="text-2xl w-full text-center my-4 font-extrabold text-primary shadow-xl shadow-black">
-		{author.name}
-	</h1>
-	<img src={author.image || dummyProfile} alt={author.name + 'cover'} class="w-2/3" />
-	<div class="flex items-center justify-around w-11/12 shadow-xl shadow-black p-4 flex-wrap">
-		<div class="flex w-48 items-center justify-between">
-			<p>Born:</p>
-			<p class="text-xl">{author.born}</p>
-		</div>
-		<div class="flex w-48 items-center justify-between">
-			<p>Died:</p>
-			<p class="text-xl">{author.died}</p>
-		</div>
-		<div class="flex w-48 items-center justify-between">
-			<p>Birth Place:</p>
-			<p class="text-xl">{author.birth_place}</p>
-		</div>
-	</div>
-	<div class="flex items-center justify-around w-11/12 shadow-xl shadow-black p-4 flex-wrap">
+	<div class="flex items-center justify-between w-11/12 my-6 p-4 shadow-xl shadow-black">
+		<h1 class="text-2xl w-full text-center font-extrabold text-primary">
+			{author.name}
+		</h1>
 		<button on:click={handleDelete} class="btn btn-error"
 			><Icon icon="mdi:delete-outline" class="text-2xl" /></button
 		>
+	</div>
+
+	<div class="flex items-center justify-center p-4 shadow-xl shadow-black gap-6 flex-wrap">
+		<img src={author.image || dummyProfile} alt={author.name + 'cover'} class="w-1/3" />
+		<div class="stats stats-vertical w-2/3">
+			<div class="stat place-items-center">
+				<div class="stat-title">Born</div>
+				<div class="stat-value">{author.born}</div>
+			</div>
+			<div class="stat place-items-center">
+				<div class="stat-title">Died</div>
+				<div class="stat-value">{author.died}</div>
+			</div>
+			<div class="stat place-items-center">
+				<div class="stat-title">Birth Place</div>
+				<div class="stat-value">{author.birth_place}</div>
+			</div>
+		</div>
 	</div>
 </div>
