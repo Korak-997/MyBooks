98081affcaa4e20396958f7ac32c4a3985597f9a
Korak Kurani <59537065+Korak-997@users.noreply.github.com>
2024-02-21 13:15:12 +0100

    Add edit and show button

--- src/lib/components/Book.svelte
+++ src/lib/components/Book.svelte
@@ -6,32 +6,33 @@
 	let language = book.language && book.language.toLowerCase();
 </script>
 
-<div class="flex items-center justify-center flex-col book">
-	<div class="book-bg w-full h-full">
-		{#if book.description}
-			<h6
-				class="absolute text-center text-neutral p-4 h-full overflow-auto"
-				class:font-kurdish={language == 'kurdish'}
-			>
-				{book.description}
-			</h6>
-		{/if}
-		<div
-			class:book-cover={book.description}
-			class="h-full w-full flex items-center justify-center z-10 text-right absolute bg-base-100"
-			style={`background-image: url(${book.cover || dummyCover}); background-size:cover; background-repeat:no-repeat; background-position:center;`}
-		/>
+<div class="flex items-center justify-center flex-col gap-4 shadow-xl shadow-black p-2">
+	<div class="p-2 flex items-center justify-center flex-col book">
+		<div class="book-bg w-full h-full">
+			{#if book.description}
+				<h6
+					class="absolute text-center text-neutral p-4 h-full overflow-auto"
+					class:font-kurdish={language == 'kurdish'}
+				>
+					{book.description}
+				</h6>
+			{/if}
+			<div
+				class:book-cover={book.description}
+				class=" h-full w-full flex items-center justify-center z-10 text-right absolute bg-base-100"
+				style={`background-image: url(${book.cover || dummyCover}); background-size:cover; background-repeat:no-repeat; background-position:center;`}
+			/>
+		</div>
+	</div>
+	<div class="flex items-center justify-around w-full">
+		<a title="show book" href={'/book/show/' + book.id} class="btn btn-secondary"
+			><Icon icon="mdi:show-outline" class="text-2xl" /></a
+		>
+		<a title="edit book" href={'/book/edit/' + book.id} class="btn btn-warning"
+			><Icon icon="material-symbols-light:edit-outline" class="text-2xl" /></a
+		>
 	</div>
 </div>
-
-<!-- <div class="flex items-center justify-around w-full">
-  <a title="show book" href={'/book/show/' + book.id} class="btn btn-secondary"
-    ><Icon icon="mdi:show-outline" class="text-2xl" /></a
-  >
-  <a title="edit book" href={'/book/edit/' + book.id} class="btn btn-warning"
-    ><Icon icon="material-symbols-light:edit-outline" class="text-2xl" /></a
-  >
-</div> -->
 <style scoped>
 	:root {
 		--width: 250px;
